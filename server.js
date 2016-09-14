0f22971db8adae0e14212e940b29037ef1196111
ishancoder <isaxena171@gmail.com>
2016-09-14 10:09:06 +0530

    Fixed issue

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var app = express();
 var re = /\([^\(\)]*\)/;
 
 app.get('/', function(req, res){
-	var ip = os = lang = null;
+	var os = lang = null;
 	os = req.headers['user-agent'].match(re)[0];
 	lang = req.headers['accept-language'].split(",")[0];
 	res.json({
-		"ipaddress":req.ip,
+		"ipaddress":req.headers['x-forwarded-for'],
 		"language": lang,
 		"software":os.substring(1,(os.length - 1))
 	});
